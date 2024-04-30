0760b363352cfaf2c2311558e573c734bc93a13f
Muhammad Imran <imran5244513@gmail.com>
2024-04-30 09:03:11 +0500

    update

--- Resume/src/assets/components/Sidebar.jsx
+++ Resume/src/assets/components/Sidebar.jsx
@@ -2,8 +2,8 @@ import imran from '../images/imran.jpg';
 function Sidebar() {
   return (
     <>
-      <div className='min-h-screen p-3 text-black'>
-        <div className='flex justify-center mt-2'>
+      <div className=' bg-blue-400 min-h-screen p-3 text-black'>
+        <div className='  flex justify-center mt-2'>
           <img src={imran} alt='imran-image' className='w-36 h-36 '></img>
         </div>
       </div>
